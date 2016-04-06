7d77e4aed85b1e7acc33dc07ac500b6fe9f5ba6c
anok <rodrigolaquino@gmail.com>
2016-04-05 23:31:47 -0400

    !loot command now records the last item

--- robin-rpg.user.js
+++ robin-rpg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin rpg bot
 // @namespace    http://tampermonkey.net/
-// @version      2.3
+// @version      2.4
 // @description  rpg bot for reddit robin ;3 based on /u/npinsker trivia bot
 // @author       /u/anokrs
 // @include      https://www.reddit.com/robin*
@@ -17,7 +17,7 @@
     var NUM_COMMANDS = 5;
     var COMMANDS_TIMEOUT = 5000;
 
-QUESTS_PER_SCORE_DISPLAY = 10;
+	var QUESTS_PER_SCORE_DISPLAY = 10;
     //var NUM_SCORES_TO_DISPLAY = 15;
 
     var NUM_TO_FLEE = 6;
@@ -76,14 +76,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
         }
         _scores[user][1] += 1;
 
-
-        if (_loot[user] === undefined) {
-            _loot[user] = "";
-        }
-        if (_loot[user].length > 0) {
-            _loot[user] += ", ";
-        }
-        _loot[user] += lootstr;
+        _loot[user] = lootstr;
 
     }
 
@@ -210,7 +203,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
         unsafeWindow.$(".text-counter-input").val(truncated_message).trigger("submit");
     }
     function printQuest(index) {
-  sendMessage(FILTER+" A wild " + _q[index].name + " appeared! HP: " + Math.round(_q[index].hp * _hpmul) + "[⬛⬛⬛⬛⬛]! Attack it by chatting! (you can !flee and get !help)! Join "+ FILTER +" for 50%+ exp!");
+        sendMessage(FILTER+" A wild " + _q[index].name + " appeared! HP: " + Math.round(_q[index].hp * _hpmul) + "[⬛⬛⬛⬛⬛]! Attack it by chatting! (you can !flee and get !help)! Check out the !commands.");
     }
 
     function renderHP(hp, hptotal) {
@@ -256,7 +249,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
 
         //only read FILTER;
         if(FILTER_CHANNEL) {
-		if(_msg.substring(0,4) !== FILTER) {
+        if(_msg.substring(0,4) !== FILTER) {
                 return true;
             }
         }
@@ -296,11 +289,11 @@ QUESTS_PER_SCORE_DISPLAY = 10;
                 continue;
             }
 
-	if(_msg.includes("!commands")) {
-		commmandsList.push(["!commands", _user]);
-		continue;
-	}
-	
+    if(_msg.includes("!commands")) {
+        commmandsList.push(["!commands", _user]);
+        continue;
+    }
+    
             if(_msg.includes("!heroes")) {
                 commmandsList.push(["!heroes", _user]);
                 continue;
@@ -324,15 +317,25 @@ QUESTS_PER_SCORE_DISPLAY = 10;
     function replyCommand() {
         setTimeout(function() {
             var commands = pullNewCommands();
-		commandMessage = FILTER+" ";
+        commandMessage = FILTER+" ";
             var commandsList = listCommands(commands);
             if(commandsList.length > 0) {
                 var command = commandsList[0][0];
                 var command_user = commandsList[0][1];
                 switch(command) {
                     case "!loot":
-                        
-                        commandMessage += command_user + " bag of holding contains " + readLootAmount(command_user) + " shiny things! " + readLootItems(command_user);
+						var _lootAmmount = readLootAmount(command_user);
+						if(_lootAmmount > 0) {
+							commandMessage += command_user + " bag of holding contains " + _lootAmmount + " shiny things! ";
+							_lootName = readLootItems(command_user);
+							if(_lootName.length > 0) {
+								commandMessage += "A [" + readLootItems(command_user) + "] surfaces in it...";
+							} else {
+								commandMessage += "Nothing is visible inside... ";
+							}
+						} else {
+							commandMessage += command_user " bag of holding is currently empty! It contains 0 shiny things!";
+						}
                         break;
                     case "!party":
                         commandMessage += assembleParty(command_user);
@@ -342,12 +345,10 @@ QUESTS_PER_SCORE_DISPLAY = 10;
                         break;
                     case "!help":
                         commandMessage += "Each line deals damage, which is based on your level and your loot, other !commands are avaliable.";
-				break;
-				case "!commands":
-					commandMessage += "!loot checks your belongings, !heroes check the hall of fame, !party check your level, !flee runs away.";
                         break;
                     case "!commands":
                         commandMessage += "!loot checks your belongings, !heroes check the hall of fame, !party check your level, !flee runs away.";
+                        break;
                 }
                 sendMessage(commandMessage);
             }
@@ -370,12 +371,11 @@ QUESTS_PER_SCORE_DISPLAY = 10;
             shuffle(partyPeople);
             partyPeople = partyPeople.slice(0,15);
             for(var i = 0; i < partyPeople.length; i++) {
-            	if(partyPeople[i][0] == user) {
-            		partyPeople = partyPeople.splice(i, 1);
-            		break;
-            	}
+                if(partyPeople[i][0] == user) {
+                    partyPeople = partyPeople.splice(i, 1);
+                    break;
+                }
             }
-		}
             reply += partyPeople.map(i => userInfoLvl(i[0])).slice(0, 15).join(", ");
         }
         return reply;
@@ -394,7 +394,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
             _round.num++;
             var answers = pullNewAnswers();
             var usersScored = judgeAnswers(answers);
-	var buildAnswerMessage = FILTER+" ";
+            var buildAnswerMessage = FILTER+" ";
             var runaway = false;
             if(_runaway >= NUM_TO_FLEE && (_round.hpleft*100/hptotal) > 70) {
                 buildAnswerMessage += "You fleed " +  _q[index].name + " and it's glorious loot of [" + _l[generateLoot()] + "]!";
@@ -421,7 +421,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
                 }
                 usersArray.sort(function(a, b) { return -(a[1] - b[1]); });
                 if(usersArray.length === 0) {
-			_runaway++;
+                    _runaway++;
                     buildAnswerMessage += "no one :(";
                 }
                 _round.party = usersArray.length > 0 ? usersArray : _round.party;
@@ -483,7 +483,7 @@ QUESTS_PER_SCORE_DISPLAY = 10;
         this.hpleft = 0;
         this.lasthit = "";
         this.party = party;
-	  this.killed = false;
+      this.killed = false;
     }
 
     function poseSeveralQuests(indices, timeout, breaktime) {
@@ -545,17 +545,17 @@ QUESTS_PER_SCORE_DISPLAY = 10;
                 _round.hpleft -= _userlevel * _lootbonus * _ratio;
                 _round.dmg += _userlevel * _lootbonus * _ratio;
             }
-	if(answers[i][1].includes(FILTER)) {
+    /*if(answers[i][1].includes(FILTER)) {
                 roundExp[_user] += (_ratio * 0.5);
                 _round.hpleft -= _userlevel * _lootbonus * (_ratio * 0.5);
                 _round.dmg += _userlevel  * _lootbonus * (_ratio * 0.5);
-            }
+            }*/
 
-	if(_round.hpleft <= 0 && _round.killed === false) {
-		console.log("kill bill");
+    if(_round.hpleft <= 0 && _round.killed === false) {
+        console.log("kill bill");
                 roundExp[_user] += 10;
                 _round.lasthit = answers[i][0];
-		_round.killed = true;
+        _round.killed = true;
             }
         }
 
